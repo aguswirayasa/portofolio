d3d9fdb6bbeab1a6bb8ed5207fa9b071f041d52f
aguswirayasa <aguswirayasa08@gmail.com>
2023-06-25 23:33:28 +0800

    adding video introduction

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -252,12 +252,21 @@ function Landing() {
             </div>
             <div className="grid w-full place-items-center my-8">
               <div className="grid grid-cols-12 w-3/5 gap-3">
-                <button className="xs:col-span-12 md:col-span-6 rounded-md px-3 py-2 font-semibold xs:text-base md:text-lg bg-purple-500 shadow-lg shadow-purple-500/50 hover:scale-105 text-white col-span-6 w-full hover:bg-purple-700 transition-all duration-200">
+                <a
+                  href="
+                "
+                  className="xs:col-span-12 md:col-span-6 rounded-md px-3 py-2 font-semibold xs:text-base md:text-lg bg-purple-500 shadow-lg shadow-purple-500/50 hover:scale-105 text-white col-span-6 w-full hover:bg-purple-700 transition-all duration-200"
+                >
                   Resume
-                </button>
-                <button className="xs:col-span-12 md:col-span-6 rounded-md px-3 py-2 font-semibold xs:text-base md:text-lg bg-purple-500 shadow-lg shadow-purple-500/50 hover:scale-105 text-white col-span-6 w-full  hover:bg-purple-700 transition-all duration-200">
+                </a>
+                <a
+                  href="https://youtu.be/1RPMbUwwmYg"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="xs:col-span-12 md:col-span-6 rounded-md px-3 py-2 font-semibold xs:text-base md:text-lg bg-purple-500 shadow-lg shadow-purple-500/50 hover:scale-105 text-white col-span-6 w-full  hover:bg-purple-700 transition-all duration-200"
+                >
                   Video Introduction
-                </button>
+                </a>
               </div>
             </div>
           </motion.div>
